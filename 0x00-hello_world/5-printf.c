8d3c8a05c6eef7c55c585d263e064359c73490b6
ZacAyman <ziadayman.mo@gmail.com>
2023-02-17 07:38:35 -0800

    new ptintf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
-#!/bin/bash
- # include <stdio.h>
- int main (void){
-	 { printf("with proper grammar, but the outcome is a piece of art,\n"); return (0); } }
+#include <stdio.h>
+/**
+* main - It is going good with printf function
+*Return:0(hello)
+**/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
