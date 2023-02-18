7aad2e2281685f879af198b8c58b72b434ac0ead
ZacAyman <ziadayman.mo@gmail.com>
2023-02-17 17:50:14 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main -A good way To code C
+* return: 0 (CISFUN)
+*/
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("size of char: %lu byte(s) / n", (unsigned long)sizeof(a));
+printf("size of int: %lu byte(s) / n", (unsigned long)sizeof(b));
+printf("size of long int: %lu byte(s) / n", (unsigned long)sizeof(c));
+printf("size of long long int: %lu byte(s) / n", (unsigned long)sizeof(d));
+printf("size of float: %lu byte(s) / n", (unsigned long)sizeof(e));
+return: (0);
+}
