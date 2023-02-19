d0ccec713078c9a255a6bd09bfda4d6c52f6af7d
ZacAyman <ziadayman.mo@gmail.com>
2023-02-19 09:17:44 -0800

    f

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-/**
-* main -A good way To code C
-* return: 0 (CISFUN)
-*/
+/*
+* main - It is going good with printf function
+*Return:0(hello)
+**/
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float e;
-printf("size of char: %lu byte(s) / n", (unsigned long)sizeof(a));
-printf("size of int: %lu byte(s) / n", (unsigned long)sizeof(b));
-printf("size of long int: %lu byte(s) / n", (unsigned long)sizeof(c));
-printf("size of long long int: %lu byte(s) / n", (unsigned long)sizeof(d));
-printf("size of float: %lu byte(s) / n", (unsigned long)sizeof(e));
-return: (0);
-}
+char d;
+int a;
+long int b;
+long long int c;
+float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));                                                     return (0);
