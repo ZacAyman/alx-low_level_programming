c6f25fe66d394cd9e3d395bba249186b78b90123
ZAC <ziadayman.mo@gmail.com>
2023-10-05 14:35:35 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
-#include <stdio.h>;
-int main (void) ;
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: A C program that prints put function
+ *
+ * Return always 0 (Success)
+*/	
+int main(void)
 {	
-	pust("Programming is like building a multilingual puzzle\n") ;
-
-	return = (0) ;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
