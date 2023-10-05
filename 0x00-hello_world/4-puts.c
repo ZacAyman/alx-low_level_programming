d59cc054dc5a6c02a08dffce41bbc838c0a547ea
ZAC <ziadayman.mo@gmail.com>
2023-10-05 14:41:48 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  * Description: A C program that prints put function
  *
  * Return always 0 (Success)
-*/	
+*/
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
